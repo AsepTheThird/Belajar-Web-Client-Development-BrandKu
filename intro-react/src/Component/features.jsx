5e0e0fa259275e689fc13803e8aa6078a0824324
Mohamad Rafli Ramadhan <rafliramadhan750@gmail.com>
2026-06-09 08:41:49 +0700

    update konten card fitur

--- intro-react/src/Component/features.jsx
+++ intro-react/src/Component/features.jsx
@@ -13,7 +13,7 @@ const Features = () => {
 
         {/*Kartu 2*/}
         <article className="rounded-xl shadow-md hover:shadow-xl hover:-translate-y-1 transition-all duration-300">
-          <div className="bg-blue-400 h-24"></div>
+          <div className="bg-blue-400 h-24 rounded-t-xl"></div>
           <div className="bg-white p-6">
             <h3 className="text-xl font-bold mb-2 text-gray-900">Serba Otomatis</h3>
             <p className="text-gray-600">Cepat digunakan, karena dibantu dengan AI.</p>
@@ -22,7 +22,7 @@ const Features = () => {
 
         {/*Kartu 3*/}
         <article className="rounded-xl shadow-md hover:shadow-xl hover:-translate-y-1 transition-all duration-300">
-          <div className="bg-blue-400 h-24"></div>
+          <div className="bg-blue-400 h-24 rounded-t-xl"></div>
           <div className="bg-white p-6">
             <h3 className="text-xl font-bold mb-2 text-gray-900">Laporan Real Time</h3>
             <p className="text-gray-600">Bisa melihat semua laporan, baik dari yang terkini hingga yang lama.</p>
